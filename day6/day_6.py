3cad4c88cf13159bcad464674d78192c4d18c42b
Rerebla <52045338+Rerebla@users.noreply.github.com>
2022-12-06 22:03:16 +0100

    Day 6 cleanup

--- day6/day_6.py
+++ day6/day_6.py
@@ -1,18 +1,11 @@
+def searchForPacket(window_size, line):
+    for i in range(len(line) - window_size + 1):
+        window = line[i: i + window_size]
+        if len(window) == len(set(window)):
+            return i + window_size
+
+
 with open('input.txt') as f:
     line = f.readline().strip()
-    window_size = 4
-
-    for i in range(len(line) - window_size + 1):
-        window = line[i: i + window_size]
-        if len(window) == len(set(window)):
-            print(window)
-            print(i + window_size)
-            break
-
-    window_size = 14
-    for i in range(len(line) - window_size + 1):
-        window = line[i: i + window_size]
-        if len(window) == len(set(window)):
-            print(window)
-            print(i + window_size)
-            break
+    print(f"Part 1: Found after: {searchForPacket(4,line)}")
+    print(f"Part 2: Found after: {searchForPacket(14,line)}")
