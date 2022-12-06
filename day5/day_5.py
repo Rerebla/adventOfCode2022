5e2435edf9e652b5e590bda72cc40e1960f80893
Rerebla <52045338+Rerebla@users.noreply.github.com>
2022-12-06 21:48:59 +0100

    Day 5 cleanup

--- day5/day_5.py
+++ day5/day_5.py
@@ -7,37 +7,38 @@ def extractDirections(direction: str):
     return int(quantity), int(origin), int(destination)
 
 
+def printSolution(cargo: list[list[str]], part: str):
+    solutionString = ""
+    for container in cargo:
+        solutionString += container[-1]
+    print(f"Part {part}: The solution is: {solutionString}")
+
+
 with open('input.txt') as f:
     lines = f.readlines()
     grid = lines[:8]
     cargo: list[list[str]] = [[] for i in range(9)]
+
     for i, line in enumerate(reversed(grid)):
         for i, match in enumerate(re.findall("\[(.*?)\]", line)):
             if match != "":
                 cargo[i].append(match)
 
+    # Part 1
     cargoPart1 = copy.deepcopy(cargo)
     directions = [line.strip() for line in lines[10:]]
     for direction in directions:
         quantity, origin, destination = extractDirections(direction)
         for i in range(0, quantity):
-            cargoPart1[destination-1]
-            cargoPart1[origin-1]
-            cargoPart1[origin-1][-1]
             cargoPart1[destination-1].append(cargoPart1[origin-1][-1])
             del cargoPart1[origin-1][-1]
-    solutionString = ""
-    for container in cargoPart1:
-        solutionString += container[-1]
-    print(solutionString)
-    # Part2
+
+    # Part 2
     cargoPart2 = copy.deepcopy(cargo)
     for direction in directions:
         quantity, origin, destination = extractDirections(direction)
         cargoPart2[destination-1].extend(cargoPart2[origin-1][-quantity:])
         del cargoPart2[origin-1][-quantity:]
 
-    solutionString = ""
-    for container in cargoPart2:
-        solutionString += container[-1]
-    print(solutionString)
+    printSolution(cargo=cargoPart1, part=1)
+    printSolution(cargo=cargoPart2, part=2)
